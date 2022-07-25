8fa14198da39bf379a06236f979547911b19fc27
VictorIyke <t-viykeosuji@microsoft.com>
2022-07-25 09:39:37 -0700

    mobile implementation

---
+++ mobile/screens/screens.tsx
@@ -0,0 +1,11 @@
+import WelcomeScreen from './welcomeScreen';
+import SuperRes from './SuperRes';
+
+
+const screens = {
+    first : WelcomeScreen,
+    second : SuperRes,
+}
+
+export default screens
+  
---
+++ mobile/screens/welcomeScreen.tsx
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Text, Button, TouchableOpacity, Alert } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
+import { RootParamList } from './NavigStack'
+
+  
+type WelcomeScreenProps = StackScreenProps<RootParamList, 'WelcomeScreen', "0">
+
+export default function WelcomeScreen({ navigation, route }: WelcomeScreenProps) {
+    return ( 
+      <View style={styles.container}>
+          <Text style = {{marginBottom: 16, margin: 24, fontSize: 18, fontWeight: "bold", textAlign: "center"}}>
+            Hello There!!! Welcome to the Super-Resolution App</Text>
+          <Button 
+              title='Go to Screen 2'
+              onPress={() => {navigation.push('Super_Resolution')}}
+          />
+  
+          <Button
+              title='Go Back'
+              onPress={() => {
+                if (navigation.canGoBack()) navigation.pop()
+                else Alert.alert("No More Screens", "There is no screen to go back to");
+              }}
+          />
+          <Button
+              title='Go to Home'
+              onPress={() => {
+                if (navigation.canGoBack()) navigation.popToTop()
+                else Alert.alert("No More Screens", "There is no screen to go back to");
+              }}
+              />
+      </View>
+    )
+  }
+
+  
+const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+},
+});
